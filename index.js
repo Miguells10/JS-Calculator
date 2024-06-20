ed8af0eb87d53320b401d402d7cddd0dddbba12e
Miguel Lucas <lucasmiguel646@gmail.com>
2024-06-20 16:31:01 -0300

    Deixando responsivo e mudando de posição os botões

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 //Calculator program
 
-const display = document.getElementById("display")
+const display = document.getElementById("display");
 
 const appendToDisplay = (input) => {
-    display.value += input
-
-}
-
+  display.value += input;
+};
 
 const clearDisplay = () => {
-    display.value = ""
+  display.value = "";
+};
 
-}
+const backspace = () => {
+    display.value = display.value.slice(0, -1);
+    }
 
 const calculate = () => {
-    try {
-    display.value = eval(display.value)
-
-} catch(error){
-    display.value = "Error"
-}
-}
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+};
