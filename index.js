89a7d5fb90cec97bea7e35c19e682957629733b5
Miguel Lucas <lucasmiguel646@gmail.com>
2024-06-20 15:25:07 -0300

    Create a calculator

---
+++ index.js
@@ -0,0 +1,23 @@
+//Calculator program
+
+const display = document.getElementById("display")
+
+const appendToDisplay = (input) => {
+    display.value += input
+
+}
+
+
+const clearDisplay = () => {
+    display.value = ""
+
+}
+
+const calculate = () => {
+    try {
+    display.value = eval(display.value)
+
+} catch(error){
+    display.value = "Error"
+}
+}
